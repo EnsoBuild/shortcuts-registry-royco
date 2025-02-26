6e6135c0de475e0738c21680008ba1c6f59e96e8
milonite <milonite@hotmail.it>
2025-02-26 13:51:47 +0100

    revert

--- src/shortcuts/origin/wos_redeem.ts
+++ src/shortcuts/origin/wos_redeem.ts
@@ -1,9 +1,9 @@
 import { Builder } from '@ensofinance/shortcuts-builder';
 import { RoycoClient } from '@ensofinance/shortcuts-builder/client/implementations/roycoClient';
-import { AddressArg, ChainIds, WeirollScript } from '@ensofinance/shortcuts-builder/types';
+import { ChainIds, WeirollScript } from '@ensofinance/shortcuts-builder/types';
 
 import { chainIdToDeFiAddresses } from '../../constants';
-import type { AddressData, Input, Output, Shortcut } from '../../types';
+import type { Input, Output, Shortcut } from '../../types';
 import { getBalance, redeemErc4626, sendTokensToOwner } from '../../utils';
 
 export class Origin_Wos_Redeem_Shortcut implements Shortcut {
@@ -29,6 +29,7 @@ export class Origin_Wos_Redeem_Shortcut implements Shortcut {
     });
     const vaultAmount = getBalance(vault, builder);
     await redeemErc4626(vault, wOS, vaultAmount, builder);
+
     const wOSAmount = getBalance(wOS, builder);
     await sendTokensToOwner(wOS, wOSAmount, builder);
 
@@ -41,17 +42,5 @@ export class Origin_Wos_Redeem_Shortcut implements Shortcut {
       script: payload.shortcut as WeirollScript,
       metadata: builder.metadata,
     };
-  }
-
-  getAddressData(chainId: number): Map<AddressArg, AddressData> {
-    switch (chainId) {
-      case ChainIds.Sonic:
-        return new Map([
-          [this.inputs[ChainIds.Sonic].wS, { label: 'ERC20:wS' }],
-          [this.inputs[ChainIds.Sonic].vault, { label: 'ERC20:Silo Vault' }],
-        ]);
-      default:
-        throw new Error(`Unsupported chainId: ${chainId}`);
-    }
   }
 }
